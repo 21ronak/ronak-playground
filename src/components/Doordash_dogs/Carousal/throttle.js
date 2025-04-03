3a14d8489d3414a872a6f2145a47a75c4dfe88e7
Ronak Maheshwari <rmaheshwari@hubspot.com>
2025-04-02 20:26:42 -0700

    add throttle

--- src/components/Doordash_dogs/Carousal/throttle.js
+++ src/components/Doordash_dogs/Carousal/throttle.js
@@ -14,3 +14,54 @@ export const throttle = (fn, delay) => {
     fn.apply(this, args);
   }
 }
+
+export const debounce = (fn, delay) => {
+  let timeoutId = null;
+
+  return function(...args) {
+    let context = this;
+
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      fn.apply(context, args);
+    }, delay);
+  }
+}
+
+/**
+ * @param {Function} func
+ * @param {number} wait
+ * @return {Function}
+ */
+export default function debounce2(func, wait) {
+  let timeoutId = null;
+  let argsToInvoke = undefined;
+  let context = undefined;
+
+  const debouncedFunction = function(...args) {
+    clearTimeout(timeoutId);
+
+    context = this;
+    argsToInvoke = args;
+    timeoutId = setTimeout(() => {
+      func.apply(context, argsToInvoke);
+      timeoutId = null;
+    }, wait);
+  };
+
+  debouncedFunction.flush = function() {
+    // Don't invoke if there's no pending callback.
+    if(timeoutId) {
+      clearTimeout(timeoutId);
+      func.apply(context, argsToInvoke);
+      timeoutId = null;
+    }
+  };
+
+  debouncedFunction.cancel =   function() {
+    clearTimeout(timeoutId);
+    timeoutId = null;
+  };
+
+  return debouncedFunction;
+}
